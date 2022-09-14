481ace608cc15bcf628f6d65912adc2a54c7f92b
Ganji Kushal <108782236+GanjiKushal@users.noreply.github.com>
2022-09-14 21:14:28 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,45 @@
+var http = require("http");
+
+// const httpServer = http.createServer(handleServer);
+
+
+// function handleServer(req, res) {
+//     // console.log(req.url,req.method,req.header)
+//     if(req.url=="/welcome"){
+//         res.writeHead(200,{"content-type":"text/html"})
+//         res.end("<h2>Welcome To Dominos</h2>")
+//     }
+//     else if(req.url=="/contact"){
+//         res.writeHead(200,{"content-type":" application/json"})
+//         res.end(JSON.stringify({phone: '18602100000',email: 'guestcaredominos@jublfood.com'}))
+//     }
+//     else{
+//         res.end("404")
+//     }
+  
+// }
+// httpServer.listen(8081,()=>{
+//     console.log("Our Serever Is Up at 8081")
+// })
+
+
+
+http.createServer((req, res)=> {
+    // console.log(req.url,req.method,req.header)
+    if(req.url=="/welcome"){
+        res.writeHead(200,{"content-type":"text/html"})
+        res.end("<h2>Welcome To Dominos</h2>")
+    }
+    else if(req.url=="/contact"){
+        res.writeHead(200,{"content-type":" application/json"})
+        res.end(JSON.stringify({phone: '18602100000',email: 'guestcaredominos@jublfood.com'}))
+    }
+    else{
+        res.end("404")
+    }
+  
+}).listen(8081,()=>{
+    console.log("Our Serever Is Up at 8081")
+})
+
+// module.exports = httpServer;
